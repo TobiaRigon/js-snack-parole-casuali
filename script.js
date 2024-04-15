a976fbd47710b9fc1cc18353f45eddcb19f9230e
TobiaRigon <rigontobia@gmail.com>
2024-04-15 18:01:28 +0200

    Print result

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // stampa in console una frase di N parole generate da un' API
 
-
-
 const apiUrl = 'https://flynn.boolean.careers/exercises/api/random/word';
 const nParole = 5;
 let frase = '';
+
+let result = document.getElementById('result');
 
 //effettua chiamata axios, inserisci la risposta nell array, per Nvolte
 
@@ -16,17 +16,22 @@ let frase = '';
         .catch((error) => {
             console.log(error);
         });
-    }
-
-
+    };
     async function creaFrase(){
         for(let i = 0; i < nParole; i++){
             frase += await creaParola() + ' ';
         }
         console.log(frase);
+        return frase;
     };
-    
-    creaFrase();
+    async function stampaFrase(){
+        let text = await creaFrase();
+        result.innerText = text;
+    };
+    stampaFrase();
+   
+   
 
+    
 
 
